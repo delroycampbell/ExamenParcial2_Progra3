4e48471e0433c72650556a434ce0aa768df59eab
delroycampbell <52224314+delroycampbell@users.noreply.github.com>
2025-12-08 18:17:38 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenParcial2_Progra3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
